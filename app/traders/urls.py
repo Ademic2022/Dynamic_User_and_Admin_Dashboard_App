13e9586a436555f05b33032a6c724ef4c9be570e
ademic2022 <adeeyomichael77@gmail.com>
2023-10-17 00:31:54 +0100

    project documentation

--- app/traders/urls.py
+++ app/traders/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .livefeed import TraderSSEView
 
 
 urlpatterns = [
@@ -8,9 +7,7 @@ urlpatterns = [
     path('simulate_trading/<str:trader_name>/', views.simulate_trading, name='simulate_trading'),
     path('lucky_trader/', views.lucky_trader, name='lucky_trader'),
     path('account/<str:trader_name>/', views.account, name='account'),
-    path('trade', views.trade, name='trade'),
-    path('sse/trader_simulation/', TraderSSEView.as_view(), name='trader_sse'),
-    # path('sse/', views.sse_view, name='sse_view'),
+    path('dashboard', views.dashboard, name='dashboard'),
 
 
 ]
